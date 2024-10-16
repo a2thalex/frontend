8c1646e151ba65bc4ca9d4cae3f39464c0515a56
gregory <gregory@aiinkwell.com>
2024-10-16 07:03:36 -0700

    Update Redux store, index.js, and App.js

--- src/App.js
+++ src/App.js
@@ -1,84 +1,29 @@
-import React, { Suspense, lazy, useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { IntlProvider } from 'react-intl';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import UploadPage from './pages/UploadPage';
 import Header from './components/Header';
-import Footer from './components/Footer';
-import ErrorBoundary from './components/ErrorBoundary';
-import LoadingIndicator from './components/LoadingIndicator';
-import { useAnalytics } from './hooks/useAnalytics';
-import useFeatureFlag from './hooks/useFeatureFlag';
-import { messages } from './i18n/messages';
-import { checkAuth } from './redux/actions/authActions';
-import './styles/global.css';
+import AudioPlayer from './components/AudioPlayer';
 import './App.css';
 
-const HomePage = lazy(() => import('./pages/HomePage'));
-const LoginPage = lazy(() => import('./pages/LoginPage'));
-const RegisterPage = lazy(() => import('./pages/RegisterPage'));
-const ProfilePage = lazy(() => import('./pages/ProfilePage'));
-const ArtistPage = lazy(() => import('./pages/ArtistPage'));
-const TrackPage = lazy(() => import('./pages/TrackPage'));
-const ArtistSignupPage = lazy(() => import('./pages/ArtistSignupPage'));
-const UploadPage = lazy(() => import('./pages/UploadPage'));
-
 function App() {
-  const [locale, setLocale] = useState('en');
-  const [darkMode, setDarkMode] = useState(false);
-  const enableDarkMode = useFeatureFlag('enableDarkMode');
-  const dispatch = useDispatch();
-  useAnalytics();
-
-  useEffect(() => {
-    dispatch(checkAuth());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (enableDarkMode && darkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  }, [darkMode, enableDarkMode]);
-
-  const handleLanguageChange = (newLocale) => {
-    setLocale(newLocale);
-  };
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <IntlProvider messages={messages[locale]} locale={locale}>
-      <ErrorBoundary>
-        <div className="App">
-          <a href="#main-content" className="skip-link">Skip to main content</a>
-          <Header 
-            locale={locale}
-            onLanguageChange={handleLanguageChange}
-            darkMode={darkMode}
-            onDarkModeToggle={toggleDarkMode}
-            enableDarkMode={enableDarkMode}
-          />
-          <main id="main-content">
-            <Suspense fallback={<LoadingIndicator />}>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/artists/:id" element={<ArtistPage />} />
-                <Route path="/tracks/:id" element={<TrackPage />} />
-                <Route path="/artist-signup" element={<ArtistSignupPage />} />
-                <Route path="/upload" element={<UploadPage />} />
-              </Routes>
-            </Suspense>
-          </main>
-          <Footer />
-        </div>
-      </ErrorBoundary>
-    </IntlProvider>
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </main>
+      <AudioPlayer />
+    </div>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,19 +3,13 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import authReducer from './reducers/authReducer';
-import artistReducer from './reducers/artistReducer';
 import trackReducer from './reducers/trackReducer';
-import feedbackReducer from './reducers/feedbackReducer';
-import userReducer from './reducers/userReducer';
-import dataReducer from './reducers/dataReducer';
+import playerReducer from './reducers/playerReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  artists: artistReducer,
   tracks: trackReducer,
-  feedback: feedbackReducer,
-  user: userReducer,
-  data: dataReducer
+  player: playerReducer,
 });
 
 const store = createStore(
